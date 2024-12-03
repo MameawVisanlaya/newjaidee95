80630d75025fb79a5931bc07826a9e1b0d5055f9
MameawVisanlaya <91720358+MameawVisanlaya@users.noreply.github.com>
2024-12-04 00:45:00 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,6 @@
-# 🎈 Blank app template
+import streamlit as st
 
-A simple Streamlit app template for you to modify!
-
-[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://blank-app-template.streamlit.app/)
-
-### How to run it on your own machine
-
-1. Install the requirements
-
-   ```
-   $ pip install -r requirements.txt
-   ```
-
-2. Run the app
-
-   ```
-   $ streamlit run streamlit_app.py
+st.title("🎈 My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
